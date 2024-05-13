897b88f147c4d142104c035db1ed8ee90979de0b
Turan Kutuev <turan.kutuev@onoisoftware.com>
2024-05-13 09:17:31 +0600

    fix: report image link

--- src/components/Reportpost.jsx
+++ src/components/Reportpost.jsx
@@ -1,42 +1,46 @@
 import {
-    Card,
-    Box,
-    CardContent,
-    CardMedia,
-    Typography,
-    CardActionArea,
+  Card,
+  Box,
+  CardContent,
+  CardMedia,
+  Typography,
+  CardActionArea,
 } from "@mui/material";
-import React from 'react'
+import React from "react";
 import { useNavigate } from "react-router-dom";
 
-const Reportpost = ({ id, title, img_before, description_before }) => {
-    const navigate = useNavigate();
+const Reportpost = ({ id, title, img, description_before }) => {
+  const navigate = useNavigate();
 
-    const navigateToReports = () => {
+  const navigateToReports = () => {
     navigate(`/Reports/${id}`);
   };
   return (
-    <Card sx={{ display: 'flex' }}>
-        <CardActionArea onClick={navigateToReports}>
-            <CardMedia
-                component="img"
-                sx={{ width: 151 }}
-                image={`https://tazalyk.fly.dev/api/files/tazalyk_reports/${id}/${img_before}`}
-                alt="Image"
-            />
-        <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-            <CardContent sx={{ flex: '1 0 auto' }}>
+    <Card sx={{ display: "flex" }}>
+      <CardActionArea onClick={navigateToReports}>
+        <CardMedia
+          component="img"
+          sx={{ width: 151 }}
+          image={`https://tazalyk.fly.dev/api/files/tazalyk_reports/${id}/${img}`}
+          alt="Image"
+        />
+        <Box sx={{ display: "flex", flexDirection: "column" }}>
+          <CardContent sx={{ flex: "1 0 auto" }}>
             <Typography pt={2} component="div" variant="h6">
-                {title}
+              {title}
             </Typography>
-            <Typography variant="subtitle1" color="text.secondary" component="div">
-                {description_before}
+            <Typography
+              variant="subtitle1"
+              color="text.secondary"
+              component="div"
+            >
+              {description_before}
             </Typography>
-            </CardContent>
+          </CardContent>
         </Box>
       </CardActionArea>
     </Card>
-  )
-}
+  );
+};
 
-export default Reportpost
+export default Reportpost;
